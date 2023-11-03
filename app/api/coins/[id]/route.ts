d3b564b39411aa4396b75658feb653042be07970
pchatzigiannis <pchatzigiannis@technor.gr>
2023-11-03 10:07:47 +0200

    Created api routes `/coins/market` and `/coins/[id]`

--- app/api/coins/[id]/route.ts
+++ app/api/coins/[id]/route.ts
@@ -3,14 +3,13 @@ import { NextRequest } from "next/server"
 export async function GET(req: NextRequest) {
     try {
       // Parse query parameters from the URL
-      const url = new URL(req.url)
-      const searchParams = new URLSearchParams(url.search)
-  
-      // Get the 'id' query parameter
-      const id = searchParams.get('id')
+      
+      const pathParts = req.nextUrl.pathname.split('/')
+
+      const id = pathParts[pathParts.length - 1]
   
       if (!id) {
-        return new Response(JSON.stringify({ error: 'Missing "id" query parameter' }), {
+        return new Response(JSON.stringify({ error: 'Missing "id" parameter' }), {
           status: 400 // Bad Request
         })
       }
@@ -29,19 +28,19 @@ export async function GET(req: NextRequest) {
     // Process and filter the data as needed
     const filteredData = {
         name: <string>data.name,
-        description: <string>data.description,
+        description: <string>data.description.en,
         currentPrice: <number>data.market_data.current_price.usd,
-        high24h: <number>data.high_24h,
-        low24h: <number>data.low_24h,
-        priceChangePercentage24h: <number>data.price_change_percentage_24h,
-        priceChangePercentage7d: <number>data.price_change_percentage_7d,
-        priceChangePercentage14d: <number>data.price_change_percentage_14d,
-        priceChangePercentage30d: <number>data.price_change_percentage_30d,
-        priceChangePercentage90d: <number>data.price_change_percentage_90d,
-        priceChangePercentage200d: <number>data.price_change_percentage_200d,
-        priceChangePercentage1y: <number>data.price_change_percentage_1y,
+        high24h: <number>data.market_data.high_24h.usd,
+        low24h: <number>data.market_data.low_24h.usd,
+        priceChangePercentage24h: <number>data.market_data.price_change_percentage_24h,
+        priceChangePercentage7d: <number>data.market_data.price_change_percentage_7d,
+        priceChangePercentage14d: <number>data.market_data.price_change_percentage_14d,
+        priceChangePercentage30d: <number>data.market_data.price_change_percentage_30d,
+        priceChangePercentage90d: <number>data.market_data.price_change_percentage_90d,
+        priceChangePercentage200d: <number>data.market_data.price_change_percentage_200d,
+        priceChangePercentage1y: <number>data.market_data.price_change_percentage_1y,
     }
-
+    console.log(filteredData)
     // Respond with the filtered data
     return new Response(JSON.stringify(filteredData), {
         status: 200,
